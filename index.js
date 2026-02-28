35c91733f5c31753d9c8b3c4cb8c502f97a47072
russianlove765-arch <russianlove765@gmail.com>
2026-02-28 15:30:49 +0530

    Update index.js

--- index.js
+++ index.js
@@ -5,9 +5,9 @@ const io = require('socket.io')(http);
 const path = require('path');
 
 const mc = require('minecraft-protocol');
-const serverHost = 'DOOMS_DAY_REBORN.aternos.me';
-const serverPort = 59173;
-const botUsername = '247_Monitor';
+const serverHost = 'TheTitansSMP.aternos.me.me';
+const serverPort = 27413;
+const botUsername = 'Alexnepalikichut';
 const reconnectInterval = 1 * 40 * 1000;
 
 let bot = null; // Initialize the bot as null
